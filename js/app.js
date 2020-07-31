09c59ed996b652a63389ae63de4fe070fbec0729
kokoNagaya <knagaya@risd.edu>
2020-08-01 00:35:58 +0800

    added javascript to h1 element in app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-  alert('jQuery works!');
-});
+// Wrap every letter in a span
+var textWrapper = document.querySelector('header h1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: 'header h1 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  
+  });
